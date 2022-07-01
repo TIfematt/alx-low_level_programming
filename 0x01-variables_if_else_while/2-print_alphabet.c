e4b1e03e084eef32362dc778eae2f24084402c04
Boluwatife <boluwatifeoladejo@gmail.com>
2022-07-01 21:28:54 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
+		return (0);
+}
+

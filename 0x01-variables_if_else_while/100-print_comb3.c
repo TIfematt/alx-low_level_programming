d41dfe0026a1f2ea549db0053cd38b937c555f56
Boluwatife <boluwatifeoladejo@gmail.com>
2022-07-02 18:12:37 +0000

    Print this combo boy!

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - The code goes here
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+
+
+				if (a == '8' && b == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

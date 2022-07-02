90927b857627efe5bc347ea4c10b0c59eba1613d
Boluwatife <boluwatifeoladejo@gmail.com>
2022-07-02 17:27:21 +0000

    Base 16 worked

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry level
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	char low;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}

daf5d184bd316a4d7e84dd058f16ea52ee5ec764
Boluwatife <boluwatifeoladejo@gmail.com>
2022-07-02 17:12:15 +0000

    Hope is for all

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'z'; d >= 'a'; d--)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}

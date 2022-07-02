6404b115e8c39bf24c69081265d7c115c737b18c
Boluwatife <boluwatifeoladejo@gmail.com>
2022-07-02 08:30:36 +0000

    Hope is for all

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print("%d\n", i);
+
+	return (0);
+}

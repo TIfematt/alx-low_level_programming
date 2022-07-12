37495eac24bc8d04bac4341bfbc082865eba3df9
Boluwatife <boluwatifeoladejo@gmail.com>
2022-07-12 17:17:28 +0000

    Baby steps

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * swap_int - A function that swaps the values of two integers
- * @*a: first intger
- * @*b: second integer
- * 
+ * @a: first intger
+ * @b: second integer
+ *
  * Return: Nothing
  */
 void swap_int(int *a, int *b)
